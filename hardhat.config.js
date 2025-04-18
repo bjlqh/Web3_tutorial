d02f7ac6ef9473119e4579d261f67f0a833e939f
liqh <lqh393@126.com>
2025-04-18 19:19:26 +0800

    project init

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+/**
+ * 完全是hardhat所用到的文件，大多数的配置信息都会添加到这个文件当中
+ */
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+};
---
+++ ignition/modules/Lock.js
@@ -0,0 +1,23 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+/**
+ * ignition是hardhat自己所创造的一套部署合约，并且调用合约的机制。
+ * 在这里去设置合约的参数，调用哪个函数。然后导出
+ * 
+ */
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const JAN_1ST_2030 = 1893456000;
+const ONE_GWEI = 1_000_000_000n;
+
+module.exports = buildModule("LockModule", (m) => {
+  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+  const lock = m.contract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  return { lock };
+});
